2c3fab99f3653f8545108aa7354aa44717f53083
GoopyDev <alann.romero@gmail.com>
2023-06-02 13:41:02 -0300

    Modificado archivo index.js para añadir variable de entorno PORT

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const { PORT } = process.env;
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { SampleDataCreate } = require("./src/SampleDataCreate.js");
@@ -7,8 +9,8 @@ conn
   .sync({ force: true })
   .then(
     // Iniciamos el servidor
-    server.listen(3001, () => {
-      console.log("%s listening at 3001"); // eslint-disable-line no-console
+    server.listen(PORT, () => {
+      console.log(`Server listening at ${PORT}`); // eslint-disable-line no-console
     })
   )
   .then(async () => {
